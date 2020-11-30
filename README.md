ebe0a5e079de806b90009b11fb1be0738693590d
Aleksandr Kalinin <7454585@gmail.com>
2020-11-30 15:03:02 +0300

    code create

--- README.md
+++ README.md
@@ -1,29 +1,40 @@
 # search-address
 
+Автозаполнение адреса и почтового индекса используя сервис https://datata.ru
+
+Установите свой токен в переменную this.token чтобы это заработало
+token можно получить на сайте https://datata.ru
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<div class="uk-container">
+			<form class="uk-form-stacked">
+				<div class="uk-form-row">
+					<label class="uk-form-label"
+						>Индекс почтового отделения</label
+					>
+					<div class="uk-form-controls">
+						<input
+							type="text"
+							name="index"
+							class="uk-input"
+							v-model="zip"
+						/>
+					</div>
+				</div>
+				<div class="uk-form-row">
+					<label class="uk-form-label">Адрес доставки</label>
+					<div class="uk-form-controls">
+						<textarea
+							name="street"
+							class="uk-textarea"
+							v-model="street"
+						></textarea>
+						<div class="highlight" v-if="finded.length">
+							<div
+								v-for="(item, index) in finded"
+								:key="index"
+								@click="selectFind(item)"
+							>
+								{{ item.value }}
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { debounce } from "lodash";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: "SearchAddress",
+	data() {
+		return {
+			url:
+				"https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address",
+			token: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", //your token
+			zip: "",
+			street: "",
+			finded: [],
+			selected: false,
+			words: 0,
+		};
+	},
+	created() {
+		this.debouncedGetAnswer = debounce(this.findAddress, 1000);
+	},
+	mounted() {},
+	watch: {
+		street(value) {
+			const vm = this;
+			if (!vm.selected && vm.words > 2) {
+				vm.debouncedGetAnswer();
+			}
+			vm.selected = false;
+			vm.words = value.length;
+		},
+	},
+	methods: {
+		clearFind() {
+			const vm = this;
+			vm.finded = [];
+			vm.words = 0;
+		},
+		selectFind(obj) {
+			const vm = this;
+			vm.selected = true;
+			vm.street = obj.value;
+			vm.zip = obj.data.postal_code;
+			vm.clearFind();
+		},
+		findAddress() {
+			const vm = this;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+			var options = {
+				method: "POST",
+				mode: "cors",
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+					Authorization: "Token " + vm.token,
+				},
+				body: JSON.stringify({ query: vm.street }),
+			};
+
+			fetch(vm.url, options)
+				.then((response) => response.json())
+				.then((result) => {
+					vm.finded = result.suggestions;
+				})
+				.catch((error) => console.error("error", error));
+		},
+	},
+};
+</script>
+<style scoped>
+.highlight {
+	border: 1px solid #ccc;
+}
+.highlight > div {
+	padding: 2px 10px;
+	cursor: pointer;
+}
+.highlight > div:hover {
+	background: #ddd;
 }
 </style>
